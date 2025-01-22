3a48aa5cd20f759a18ce0b00a0231d79da677a88
Chuian <chuiantw1212@gmail.com>
2025-01-22 14:47:22 +0800

    wip

--- components/Atom/VotionCalendar.vue
+++ components/Atom/VotionCalendar.vue
@@ -5,14 +5,26 @@
 <script setup lang="ts">
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
 
 const calendarOptions = reactive({
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
     weekends: false,
     events: [
         { title: 'Meeting', start: new Date() }
-    ]
+    ],
+    dateClick: function (info: any) {
+        /**
+         * 選取日期
+         * https://fullcalendar.io/docs/dateClick
+         */
+        // alert('Clicked on: ' + info.dateStr);
+        // alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+        // alert('Current view: ' + info.view.type);
+        // change the day's background color just for fun
+        info.dayEl.style.backgroundColor = 'red';
+    }
 })
 
 function locateToday() {
