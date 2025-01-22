735855274c0fd43513d040fad5c94ce5c6b94dee
Chuian <chuiantw1212@gmail.com>
2025-01-22 15:01:18 +0800

    add menu

---
+++ pages/event.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>事件管理</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,11 @@
 <template>
-  <VotionCalendar></VotionCalendar>
+  <div>首頁</div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'default',
 })
-
-import VotionCalendar from '~/components/Atom/VotionCalendar.vue';
-
 </script>
 
-<style>
-.page-index {
-  padding-top: 60px;
-  text-align: center;
-}
-</style>
+<style lang="scss" scoped></style>
