4634db6200f772ccc521092af7a32de1b4f3398b
Chuian <chuiantw1212@gmail.com>
2025-01-23 14:46:28 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="votion">
     <div class="votion__header">
-      <el-dropdown>
-        <span class="el-dropdown-link">
-          Dropdown List
-          <el-icon class="el-icon--right">
-            <arrow-down />
-          </el-icon>
-        </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>Action 1</el-dropdown-item>
-            <el-dropdown-item>Action 2</el-dropdown-item>
-            <el-dropdown-item>Action 3</el-dropdown-item>
-            <el-dropdown-item disabled>Action 4</el-dropdown-item>
-            <el-dropdown-item divided>Action 5</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-
-      </el-dropdown>
+      <el-form :style="{ 'width': '100%' }">
+        <el-row justify="end">
+          <el-form-item label="視圖切換">
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                月曆
+                <el-icon class="el-icon--right">
+                  <arrow-down />
+                </el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>網格</el-dropdown-item>
+                  <el-dropdown-item>列表</el-dropdown-item>
+                  <el-dropdown-item>月曆</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </el-form-item>
+        </el-row>
+      </el-form>
     </div>
     <!-- <div class="votion__container">
       <img src="@/assets/mock/活動月曆.jpg">
