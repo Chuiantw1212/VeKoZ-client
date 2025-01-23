e53f17a82432ca82e2da751898c17db2987bdee9
Chuian <chuiantw1212@gmail.com>
2025-01-24 07:46:30 +0800

    add auth

---
+++ pages/auth.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>權限管理</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>姓名</th>
+                <th>email</th>
+                <th>權限(下拉選單)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>EN Chu</th>
+                <th>chuinatw1212@gmail.com</th>
+                <th>創建事件</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
