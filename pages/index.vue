8736396c3a04504bfc4cc0e9d3c48d3b66033f87
Chuian <chuiantw1212@gmail.com>
2025-01-23 14:40:54 +0800

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,50 @@
 <template>
-  <div>
-    <h1>Votion 開課通</h1>
-    <img src="@/assets/mock/首頁.png">
+  <div class="votion">
+    <div class="votion__header">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          Dropdown List
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item divided>Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+
+      </el-dropdown>
+    </div>
+    <!-- <div class="votion__container">
+      <img src="@/assets/mock/活動月曆.jpg">
+    </div> -->
+    <VotionCalendar></VotionCalendar>
   </div>
 </template>
 
 <script setup lang="ts">
+import VotionCalendar from '~/components/Atom/VotionCalendar.vue';
+
 // definePageMeta({
 //   layout: 'default',
 // })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.votion {
+  .votion__header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .votion__container {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
