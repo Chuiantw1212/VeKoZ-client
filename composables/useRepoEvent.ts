b093a2039463a3fd7408756cb78c0a9dc744c7f5
EN Chu <chuiantw1212@gmail.com>
2025-01-24 16:26:02 +0800

    remove unused codes

--- composables/useRepoEvent.ts
+++ composables/useRepoEvent.ts
@@ -3,7 +3,7 @@ import useVenoniaApi from './useVenoniaApi'
 
 export default defineStore('event', () => {
     const defaultApi = useVenoniaApi()
-    
+
     async function putEventTemplate(data: any) {
         const response = await defaultApi.request(`/event/template`, {
             method: 'put',
