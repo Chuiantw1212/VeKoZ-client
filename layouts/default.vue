5bec2f6842a203a27ca9048bf819722c58d643bf
EN Chu <chuiantw1212@gmail.com>
2025-02-04 10:22:46 +0800

    add i18n

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,33 @@
 <template>
   <div class="defaultLayout">
-    <el-container>
-      <el-header>
-        <HeaderMenu></HeaderMenu>
-      </el-header>
+    <el-config-provider :locale="zhTw">
       <el-container>
-        <el-aside class="defaultLayout__aside">
-          <SideMenu></SideMenu>
-        </el-aside>
+        <el-header>
+          <HeaderMenu></HeaderMenu>
+        </el-header>
         <el-container>
-          <el-main class="defaultLayout__main">
-            <slot></slot>
-          </el-main>
+          <el-aside class="defaultLayout__aside">
+            <SideMenu></SideMenu>
+          </el-aside>
+          <el-container>
+            <el-main class="defaultLayout__main">
+              <slot></slot>
+            </el-main>
+          </el-container>
         </el-container>
       </el-container>
-    </el-container>
+    </el-config-provider>
   </div>
 </template>
 <script setup lang="ts">
 import HeaderMenu from '~/components/organism/HeaderMenu.vue'
 import SideMenu from '~/components/organism/SideMenu.vue';
+import zhTw from 'element-plus/dist/locale/zh-tw.mjs'
 </script>
 <style lang="scss" scoped>
 .defaultLayout {
   .defaultLayout__aside {
     width: fit-content;
-    // transition: all 0.3s;
   }
-}
-
-.defaultLayout__main {
-  // >* {
-  //   margin: auto;
-  //   display: flex;
-  //   // justify-content: center;
-  //   flex-direction: column;
-  //   align-items: center;
-  // }
 }
 </style>
