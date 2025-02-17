a42f7f54ddfee8eb9da5648bfdcd913db1d2831b
EN Chu <chuiantw1212@gmail.com>
2025-02-17 22:51:58 +0800

    wip

--- app.vue
+++ app.vue
@@ -1,26 +1,21 @@
 <template>
-    <div class="defaultLayout">
-        <!-- <el-config-provider :locale="zhTw"> -->
+    <el-header v-if="repoUser.userType === 'host' || repoUI.isLarge">  
+        <!-- v-if="repoUser.userType === 'host' || repoUI.isLarge" -->
+        <HeaderMenu></HeaderMenu>
+    </el-header>
+    <el-container class="defaultLayout" :class="{'defaultLayout__attendee': true}">
+        <el-aside v-if="repoUser.userType === 'host'" class="defaultLayout__aside">
+            <SideMenu></SideMenu>
+        </el-aside>
         <el-container>
-            <el-header v-if="repoUser.userType === 'host' || repoUI.isLarge">  
-                <!-- v-if="repoUser.userType === 'host' || repoUI.isLarge" -->
-                <HeaderMenu></HeaderMenu>
-            </el-header>
-            <el-container class="defaultLayout__main">
-                <el-aside v-if="repoUser.userType === 'host'" class="defaultLayout__aside">
-                    <SideMenu></SideMenu>
-                </el-aside>
-                <el-container>
-                    <el-main >
-                        <NuxtPage />
-                    </el-main>
-                </el-container>
-            </el-container>
-            <el-footer v-if="repoUser.userType !== 'host' && !repoUI.isLarge" class="defaultLayout__footer">
-                <OrganismAttendeeMenu></OrganismAttendeeMenu>
-            </el-footer>
+            <el-main >
+                <NuxtPage />
+            </el-main>
         </el-container>
-    </div>
+    </el-container>
+    <el-footer v-if="repoUser.userType !== 'host' && !repoUI.isLarge" class="defaultLayout__footer">
+        <OrganismAttendeeMenu></OrganismAttendeeMenu>
+    </el-footer>
 </template>
 <script setup lang="ts">
 import HeaderMenu from '~/components/organism/HeaderMenu.vue'
@@ -32,20 +27,23 @@ const repoUI = useRepoUI()
 </script>
 <style lang="scss" scoped>
 .defaultLayout {
+    justify-content: center;
+
     .defaultLayout__aside {
         width: fit-content;
     }
+}
 
-    .defaultLayout__main{
-        max-width: 1280px; // ?
-        margin:auto;
-    }
+.defaultLayout__attendee{
+    max-width: 1280px; // ?
+    justify-content: center;
+    margin: auto;
+}
 
-    .defaultLayout__footer {
-        position: fixed;
-        bottom: 0px;
-        width: 100vw;
-        padding: 0px;
-    }
+.defaultLayout__footer {
+    position: fixed;
+    bottom: 0px;
+    width: 100vw;
+    padding: 0px;
 }
 </style>
