e2ad918b038d59d7cd3a32bab455f7ac3463c00b
EN Chu <chuiantw1212@gmail.com>
2025-02-22 12:20:29 +0800

    update logo

--- components/organism/HeaderMenu.vue
+++ components/organism/HeaderMenu.vue
@@ -8,12 +8,12 @@
         </el-menu-item> -->
         <NuxtLink v-if="repoUser.userType === 'host'" to="/host">
             <el-menu-item class="headerMenu__logo">
-                <img src="@/assets/logo/480_320_admin.png" class="logo__image" alt="Element logo" />
+                <img src="@/assets/logo/140_60_admin.png" class="logo__image" alt="Element logo" />
             </el-menu-item>
         </NuxtLink>
         <NuxtLink v-else to="/">
             <el-menu-item class="headerMenu__logo">
-                <img src="@/assets/logo/480_320.png" class="logo__image" alt="Element logo" />
+                <img src="@/assets/logo/140_60.png" class="logo__image" alt="Element logo" />
             </el-menu-item>
         </NuxtLink>
         <div v-if="repoUser.userType === 'attendee' && repoUI.isLarge" class="attendee__menu">
--- pages/host/offer.vue
+++ pages/host/offer.vue
@@ -2,10 +2,16 @@
     <!-- <h1>票券銷售分派</h1> -->
     <!-- Todo：等後台開發。
     票券可以不公開 -->
+    <!-- {{ offerGroups }} -->
     <el-card>
         <template #header>
             投資進階管道 2025/02/22
         </template>
+        <el-form>
+            <el-firm-item label="主辦">
+                {{}}
+            </el-firm-item>
+        </el-form>
         <el-table :data="offers" style="width: 100%">
             <!-- <el-table-column prop="validFrom" label="日期">
                 <template #default="{ row }">
@@ -30,11 +36,14 @@
 import type { IOffer } from '~/types/offer';
 const repoOffer = useRepoOffer()
 const offers = ref<IOffer[]>()
+const offerGroups = ref<any>({})
 // const repoUser = useRepouser()
 
 async function getOfferList() {
     const result: IOffer[] = await repoOffer.getOfferList()
     offers.value = result
+
+    offerGroups.value = Object.groupBy(result, ({ eventId }) => eventId)
 }
 
 onMounted(() => {
