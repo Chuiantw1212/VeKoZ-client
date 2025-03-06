4b69a1674a1c68cd4bc18bfa9f07e1ce2edae7cf
EN Chu <chuiantw1212@gmail.com>
2025-03-06 21:11:44 +0800

    wip

--- components/atom/AvatarUploader/index.vue
+++ components/atom/AvatarUploader/index.vue
@@ -7,15 +7,15 @@
             </div>
         </div>
         <label class="inputGroup__label" :class="{ 'inputGroup__label--disabled': disabled, }">
-            <div v-if="localValue" class="label__image"
-            :style="{ width: size, height: size, 'background-image': getImageSrc() }"></div>
-            <img v-else class="label__image" :style="{ width: size, height: size, }" :src="placeholder">
+            <img v-if="!localValue || !localValue.type" class="label__image" :style="{ width: size, height: size, }"
+                :src="placeholder">
+            <div v-else class="label__image" :style="{ width: size, height: size, 'background-image': getImageSrc() }">
+            </div>
             <img class="label__placeholder" :style="{ width: size, height: size, }" src="./Rectangle1348.png">
             <input v-show="false" class="body__input" autocomplete="off" type="file" accept="image/*"
-            :data-required="required" :data-name="name" @change="handleFiles($event)" :disabled="disabled"></input>
+                :data-required="required" :data-name="name" @change="handleFiles($event)" :disabled="disabled"></input>
         </label>
     </div>
-    <!-- localValue{{ localValue }} -->
 </template>
 <script setup lang="ts">
 import isURL from 'validator/lib/isURL'
