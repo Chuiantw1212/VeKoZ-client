14ea91a50a720a89ee80f0f8612c69a28084dfaf
EN Chu <chuiantw1212@gmail.com>
2025-03-07 17:40:59 +0800

    delete own template

--- components/form/TemplateLoading.vue
+++ components/form/TemplateLoading.vue
@@ -127,14 +127,12 @@ async function selectTemplate(template: IEventTemplate) {
 }
 
 async function deleteTemplate(template: IEventTemplate) {
-    if (template.organizationId && template.id) {
-        isLoading.value = true
-        await repoEventTemplate.deleteEventTemplate({
-            organizationId: template.organizationId,
-            id: template.id
-        })
-        getEventTemplateList()
-    }
+    isLoading.value = true
+    await repoEventTemplate.deleteEventTemplate({
+        organizationId: template.organizationId,
+        id: template.id
+    })
+    getEventTemplateList()
 }
 
 async function getEventTemplateList() {
